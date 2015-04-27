ce6aa8ecaea3db378ab194be10503ac637b6e6fb
Satish Viswanathan <satish@INSVISWAN1.local>
2015-04-27 20:35:08 +0530

    Line Follower Robot code

--- main.c
+++ main.c
@@ -1,40 +1,123 @@
+//
+//  ObstacleAvoider.c
+//  MCLineFollower
+//
+//
+//
+
+#include <stdio.h>
 #include <avr/io.h>
 #include <util/delay.h>
 
-int main (void)
-{
-    DDRD = 0b00000000;       //Set DDRD as input part
-    PORTD = 0b11111111;      // Pull up DDRD Pins
-    DDRB = 0b11111111;       // Set DDRB as output part
-    DDRA = 0b11111111;      // Set DDRA as output part
+void takeReverse(void);
+void goForward(void);
+void turnLeft(void);
+void turnRight(void);
+void longDelay(void);
+void shortDelay(void);
+
+
+int main(void){
+    
+    DDRD = 0b00000000;   //setting part D as input part
+    DDRB = 0b11111111;   //setting part B as output part
+    PORTD = 0b11111111;  //pulling up part D
     
     int c;
     
-    while(1)
-    {
+    while(1){
         c = PIND;
-        if (c == 0b11111100)        //If both sensors are low, go straight
-        {
-            PORTB = 0b00001010;
-            PORTA = 0b00000000;
-        }
-        else if (c == 0b11111111)
-        {
-            PORTB = 0b00001010;
-            PORTA = 0b00000100;
-        }
-        else if (c == 0b11111110)            //If left sensor is high, turn left (left motor - rev, right motor - fwd)
-        {
-            PORTB = 0b00000110;
-            PORTA = 0b00000010;
-            
-        }
-        else if (c == 0b11111101)         //If right sensor is high, turn right (left motor - fwd, right motor - rev)
-        {
-            PORTB = 0b00001001;
-            PORTA = 0b00000001;
+        
+//        if (c == 0b11110000){   //All sensors within the table OR two back sensors outside the table; go fwd
+//            goForward();
+//        }
+//
+//        if (c == 0b11110011){   //All sensors within the table OR two back sensors outside the table; go fwd
+//            goForward();
+//        }
+
+        if (c == 0b11111100){   //Both front sensors out of the table; go back and then turn right
+            takeReverse();
+            turnRight();
+            shortDelay();
         }
 
+//
+//        if (c == 0b11110001){   //Back left sensor out of the table; go fwd and then turn left
+//            goForward();
+//            turnLeft();
+//            shortDelay();
+//        }
+//        
+//        if (c == 0b11110010){   //Back right sensor out of the table; go fwd and then turn right
+//            goForward();
+//            turnRight();
+//            shortDelay();
+//        }
+//
+//        if (c == 0b11110100){   //Front Right sensor out of the table; go back and then turn right to align
+//            takeReverse();
+//            turnLeft();
+//            shortDelay();
+//        }
+//        
+//        if (c == 0b11111000){   //Front Left sensor out of the table; go back and then turn left to align
+//            takeReverse();
+//            turnRight();
+//            shortDelay();
+//        }
+//
+//        if (c == 0b111101001){   //Both Left sensors out of the table; Turn right
+//            turnRight();
+//        }
+//
+//        if (c == 0b11110110){   //Both Right sensors out of the table; Turn left
+//            turnLeft();
+//        }
+//
+//
+//        if (c == 0b11110111){   //Both back sensors & front right sensor out of the table; go fwd and then turn left
+//            turnLeft();
+//        }
+//
+//        if (c == 0b11111011){   //Both back sensors & front left sensor out of the table; go fwd and then turn right
+//            turnRight();
+//        }
+//
+//        if (c == 0b11111101){   //Both front sensors & back left sensor out of the table; go back and then turn right
+//            turnRight();
+//        }
+//
+//        if (c == 0b11111101){   //Both front sensors & back right sensor out of the table; go back and then turn left
+//            turnLeft();
+//        }
+        
     }
-    
 }
+
+
+void takeReverse(void){
+    PORTB = 0b00000101;
+    longDelay();
+}
+
+void goForward(void){
+    PORTB = 0b00001010;
+}
+
+void turnLeft(void){
+    PORTB = 0b00000110;
+}
+
+void turnRight(void){
+    PORTB = 0b00001001;
+}
+
+void longDelay(void) {
+    _delay_ms(1000);
+}
+
+void shortDelay(void) {
+    _delay_ms(200);
+}
+
